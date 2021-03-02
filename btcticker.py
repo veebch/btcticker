f3ac7ab30d90d6b5cd3abfa68d7df601fcc40faf
Estacaco <44181620+Estacaco@users.noreply.github.com>
2021-03-02 09:44:51 -0600

    updated mask to match Main

--- btcticker.py
+++ btcticker.py
@@ -162,7 +162,7 @@ def updateDisplay(config,pricestack,whichcoin,fiat,other):
 #   Check for token image, if there isn't one, get on off coingecko, resize it and pop it on a white background
     if os.path.isfile(tokenfilename):
         logging.info("Getting token Image from Image directory")
-        tokenimage = Image.open(tokenfilename)
+        tokenimage = Image.open(tokenfilename).convert("RGBA")
     else:
         logging.info("Getting token Image from Coingecko")
         tokenimageurl = "https://api.coingecko.com/api/v3/coins/"+whichcoin+"?tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false"
