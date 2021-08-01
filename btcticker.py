57c3bc3151eaf2aa2e4cf015bd4f629f839c18e9
Thorsten Berberich <35310270+thorstenbe@users.noreply.github.com>
2021-08-01 19:18:15 +0200

    Make log level configurable by command line argument and adapt log level of messages

--- btcticker.py
+++ btcticker.py
@@ -14,9 +14,10 @@ import urllib, json
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
-import yaml 
+import yaml
 import socket
 import textwrap
+import argparse
 dirname = os.path.dirname(__file__)
 picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'images')
 fontdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fonts/googlefonts')
@@ -81,7 +82,7 @@ def getgecko(url):
         geckojson=requests.get(url, headers=headers).json()
         connectfail=False
     except requests.exceptions.RequestException as e:
-        logging.info("Issue with CoinGecko")
+        logging.error("Issue with CoinGecko")
         connectfail=True
         geckojson={}
     return geckojson, connectfail
@@ -94,19 +95,19 @@ def getData(config,other):
     num_retries = 5
     whichcoin,fiat=configtocoinandfiat(config)
     logging.info("Getting Data")
-    days_ago=int(config['ticker']['sparklinedays'])   
+    days_ago=int(config['ticker']['sparklinedays'])
     endtime = int(time.time())
     starttime = endtime - 60*60*24*days_ago
     starttimeseconds = starttime
-    endtimeseconds = endtime 
+    endtimeseconds = endtime
     geckourlhistorical = "https://api.coingecko.com/api/v3/coins/"+whichcoin+"/market_chart/range?vs_currency="+fiat+"&from="+str(starttimeseconds)+"&to="+str(endtimeseconds)
-    logging.info(geckourlhistorical)
-    for x in range(0, num_retries):   
+    logging.debug(geckourlhistorical)
+    for x in range(0, num_retries):
         rawtimeseries, connectfail=  getgecko(geckourlhistorical)
         if connectfail== True:
             pass
         else:
-            logging.info("Got price for the last "+str(days_ago)+" days from CoinGecko")
+            logging.debug("Got price for the last "+str(days_ago)+" days from CoinGecko")
             timeseriesarray = rawtimeseries['prices']
             timeseriesstack = []
             length=len (timeseriesarray)
@@ -115,16 +116,16 @@ def getData(config,other):
                 timeseriesstack.append(float (timeseriesarray[i][1]))
                 i+=1
             # A little pause before hiting the api again
-            time.sleep(1)          
-            # Get the price 
+            time.sleep(1)
+            # Get the price
         if config['ticker']['exchange']=='default':
             geckourl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency="+fiat+"&ids="+whichcoin
-            logging.info(geckourl)
+            logging.debug(geckourl)
             rawlivecoin , connectfail = getgecko(geckourl)
             if connectfail==True:
                 pass
             else:
-                logging.info(rawlivecoin[0])   
+                logging.debug(rawlivecoin[0])
                 liveprice = rawlivecoin[0]
                 pricenow= float(liveprice['current_price'])
                 alltimehigh = float(liveprice['ath'])
@@ -142,7 +143,7 @@ def getData(config,other):
                     other['ATH']=False
         else:
             geckourl= "https://api.coingecko.com/api/v3/exchanges/"+config['ticker']['exchange']+"/tickers?coin_ids="+whichcoin+"&include_exchange_logo=false"
-            logging.info(geckourl)
+            logging.debug(geckourl)
             rawlivecoin, connectfail = getgecko(geckourl)
             if connectfail==True:
                 pass
@@ -153,17 +154,17 @@ def getData(config,other):
                     target=rawlivecoin['tickers'][i]['target']
                     if target==upperfiat:
                         theindex=i
-                        logging.info("Found "+upperfiat+" at index " + str(i))
+                        logging.debug("Found "+upperfiat+" at index " + str(i))
         #       if UPPERFIAT is not listed as a target theindex==-1 and it is time to go to sleep
                 if  theindex==-1:
-                    logging.info("The exchange is not listing in "+upperfiat+". Misconfigured - shutting down script")
+                    logging.error("The exchange is not listing in "+upperfiat+". Misconfigured - shutting down script")
                     sys.exit()
                 liveprice= rawlivecoin['tickers'][theindex]
                 pricenow= float(liveprice['last'])
                 other['market_cap_rank'] = 0 # For non-default the Rank does not show in the API, so leave blank
                 other['volume'] = float(liveprice['converted_volume']['usd'])
                 alltimehigh = 1000000.0   # For non-default the ATH does not show in the API, so show it when price reaches *pinky in mouth* ONE MILLION DOLLARS
-                logging.info("Got Live Data From CoinGecko")
+                logging.debug("Got Live Data From CoinGecko")
                 timeseriesstack.append(pricenow)
                 if pricenow>alltimehigh:
                     other['ATH']=True
@@ -171,7 +172,7 @@ def getData(config,other):
                     other['ATH']=False
         if connectfail==True:
             message="Trying again in ", sleep_time, " seconds"
-            logging.info(message)
+            logging.warn(message)
             time.sleep(sleep_time)  # wait before trying to fetch the data again
             sleep_time *= 2  # exponential backoff
         else:
@@ -192,7 +193,7 @@ def makeSpark(pricestack):
     # Draw and save the sparkline that represents historical data
     # Subtract the mean from the sparkline to make the mean appear on the plot (it's really the x axis)
     themean= sum(pricestack)/float(len(pricestack))
-    x = [xx - themean for xx in pricestack] 
+    x = [xx - themean for xx in pricestack]
     fig, ax = plt.subplots(1,1,figsize=(10,3))
     plt.plot(x, color='k', linewidth=6)
     plt.plot(len(x)-1, x[-1], color='r', marker='o')
@@ -206,14 +207,14 @@ def makeSpark(pricestack):
     plt.savefig(os.path.join(picdir,'spark.png'), dpi=17)
     imgspk = Image.open(os.path.join(picdir,'spark.png'))
     file_out = os.path.join(picdir,'spark.bmp')
-    imgspk.save(file_out) 
+    imgspk.save(file_out)
     plt.clf() # Close plot to prevent memory error
     ax.cla() # Close axis to prevent memory error
     imgspk.close()
     return
 
 def updateDisplay(config,pricestack,other):
-    """   
+    """
     Takes the price data, the desired coin/fiat combo along with the config info for formatting
     if config is re-written following adustment we could avoid passing the last two arguments as
     they will just be the first two items of their string in config
@@ -224,7 +225,7 @@ def updateDisplay(config,pricestack,other):
     originalcoin_list = originalcoin.split(",")
     originalcoin_list = [x.strip(' ') for x in originalcoin_list]
     whichcoin,fiat=configtocoinandfiat(config)
-    days_ago=int(config['ticker']['sparklinedays'])   
+    days_ago=int(config['ticker']['sparklinedays'])
     symbolstring=currency.symbol(fiat.upper())
     if fiat=="jpy" or fiat=="cny":
         symbolstring="¥"
@@ -238,23 +239,23 @@ def updateDisplay(config,pricestack,other):
     ATHbitmap= Image.open(os.path.join(picdir,'ATH.bmp'))
 #   Check for token image, if there isn't one, get on off coingecko, resize it and pop it on a white background
     if os.path.isfile(tokenfilename):
-        logging.info("Getting token Image from Image directory")
+        logging.debug("Getting token Image from Image directory")
         tokenimage = Image.open(tokenfilename).convert("RGBA")
     else:
-        logging.info("Getting token Image from Coingecko")
+        logging.debug("Getting token Image from Coingecko")
         tokenimageurl = "https://api.coingecko.com/api/v3/coins/"+whichcoin+"?tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false"
         rawimage = requests.get(tokenimageurl, headers=headers).json()
         tokenimage = Image.open(requests.get(rawimage['image']['large'], headers = headers, stream=True).raw).convert("RGBA")
         resize = 100,100
         tokenimage.thumbnail(resize, Image.ANTIALIAS)
-        # If inverted is true, invert the token symbol before placing if on the white BG so that it is uninverted at the end - this will make things more 
+        # If inverted is true, invert the token symbol before placing if on the white BG so that it is uninverted at the end - this will make things more
         # legible on a black display
         if config['display']['inverted'] == True:
             #PIL doesnt like to invert binary images, so convert to RGB, invert and then convert back to RGBA
             tokenimage = ImageOps.invert( tokenimage.convert('RGB') )
             tokenimage = tokenimage.convert('RGBA')
         new_image = Image.new("RGBA", (120,120), "WHITE") # Create a white rgba background with a 10 pixel border
-        new_image.paste(tokenimage, (10, 10), tokenimage)   
+        new_image.paste(tokenimage, (10, 10), tokenimage)
         tokenimage=new_image
         tokenimage.thumbnail((100,100),Image.ANTIALIAS)
         tokenimage.save(tokenfilename)
@@ -270,7 +271,7 @@ def updateDisplay(config,pricestack,other):
         pricenowstring =str(float('%.5g' % pricenow))
     if config['display']['orientation'] == 0 or config['display']['orientation'] == 180 :
         image = Image.new('L', (176,264), 255)    # 255: clear the image with white
-        draw = ImageDraw.Draw(image)              
+        draw = ImageDraw.Draw(image)
         draw.text((110,80),str(days_ago)+"day :",font =font_date,fill = 0)
         draw.text((110,95),pricechange,font =font_date,fill = 0)
         writewrappedlines(image, symbolstring+pricenowstring,40,65,8,10,"Roboto-Medium" )
@@ -281,9 +282,9 @@ def updateDisplay(config,pricestack,other):
             image=image.rotate(180, expand=True)
     if config['display']['orientation'] == 90 or config['display']['orientation'] == 270 :
         image = Image.new('L', (264,176), 255)    # 255: clear the image with white
-        draw = ImageDraw.Draw(image) 
+        draw = ImageDraw.Draw(image)
         if other['ATH']==True:
-            image.paste(ATHbitmap,(190,85))  
+            image.paste(ATHbitmap,(190,85))
         draw.text((110,90),str(days_ago)+" day : "+pricechange,font =font_date,fill = 0)
         if 'showvolume' in config['display'] and config['display']['showvolume']:
             draw.text((110,105),"24h vol : " + human_format(other['volume']),font =font_date,fill = 0)
@@ -291,7 +292,7 @@ def updateDisplay(config,pricestack,other):
         writewrappedlines(image, symbolstring+pricenowstring,50,55,8,10,"Roboto-Medium" )
         image.paste(sparkbitmap,(80,40))
         image.paste(tokenimage, (0,10))
-        # Don't show rank for #1 coin, #1 doesn't need to show off                  
+        # Don't show rank for #1 coin, #1 doesn't need to show off
         if 'showrank' in config['display'] and config['display']['showrank'] and other['market_cap_rank'] > 1:
             draw.text((10,105),"Rank: " + str("%d" % other['market_cap_rank']),font =font_date,fill = 0)
         if (config['display']['trendingmode']==True) and not (str(whichcoin) in originalcoin_list):
@@ -302,7 +303,7 @@ def updateDisplay(config,pricestack,other):
             image=image.rotate(180, expand=True)
 #       This is a hack to deal with the mirroring that goes on in older waveshare libraries Uncomment line below if needed
 #       image = ImageOps.mirror(image)
-#   If the display is inverted, invert the image usinng ImageOps        
+#   If the display is inverted, invert the image usinng ImageOps
     if config['display']['inverted'] == True:
         image = ImageOps.invert(image)
 #   Return the ticker image
@@ -318,11 +319,11 @@ def currencycycle(curr_string):
     curr_list=currencystringtolist(curr_string)
     # Rotate the array of currencies from config.... [a b c] becomes [b c a]
     curr_list = curr_list[1:]+curr_list[:1]
-    return curr_list    
+    return curr_list
 
 def display_image(img):
 
-    epd = epd2in7.EPD()  
+    epd = epd2in7.EPD()
     epd.Init_4Gray()
     epd.display_4Gray(epd.getbuffer_4Gray(img))
     epd.sleep()
@@ -344,19 +345,19 @@ def initkeys():
     return thekeys
 
 def configwrite(config):
-    """  
+    """
         Write the config file following an adjustment made using the buttons
-        This is so that the unit returns to its last state after it has been 
-        powered off 
-    """ 
+        This is so that the unit returns to its last state after it has been
+        powered off
+    """
     with open(configfile, 'w') as f:
         data = yaml.dump(config, f)
 
 def fullupdate(config,lastcoinfetch):
-    """  
+    """
     The steps required for a full update of the display
     Earlier versions of the code didn't grab new data for some operations
-    but the e-Paper is too slow to bother the coingecko API 
+    but the e-Paper is too slow to bother the coingecko API
     """
     other={}
     try:
@@ -392,16 +393,18 @@ def gettrending(config):
     trendingcoins = requests.get(url, headers=headers).json()
     for i in range(0,(len(trendingcoins['coins']))):
         print(trendingcoins['coins'][i]['item']['id'])
-        coinlist+=","+str(trendingcoins['coins'][i]['item']['id'])  
+        coinlist+=","+str(trendingcoins['coins'][i]['item']['id'])
     config['ticker']['currency']=coinlist
     return config
 
-def main(loglevel=logging.WARNING):    
-    # To debug, uncomment this line (default is warning)
-    loglevel = logging.DEBUG
-    
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--log", default='info', help='Set the log level (default: info)')
+    args = parser.parse_args()
+
+    loglevel = getattr(logging, args.log.upper(), logging.WARN)
     logging.basicConfig(level=loglevel)
-    
+
     try:
         logging.info("epd2in7 BTC Frame")
 #       Get the configuration from config.yaml
@@ -415,12 +418,12 @@ def main(loglevel=logging.WARNING):
 #       Note how many coins in original config file
         howmanycoins=len(config['ticker']['currency'].split(","))
 #       Note that there has been no data pull yet
-        datapulled=False 
+        datapulled=False
 #       Time of start
         lastcoinfetch = time.time()
 #       Quick Sanity check on update frequency, waveshare says no faster than 180 seconds, but we'll make 60 the lower limit
         if float(config['ticker']['updatefrequency'])<60:
-            logging.info("Throttling update frequency to 60 seconds")  
+            logging.info("Throttling update frequency to 60 seconds")
             updatefrequency=60.0
         else:
             updatefrequency=float(config['ticker']['updatefrequency'])
@@ -432,13 +435,13 @@ def main(loglevel=logging.WARNING):
             key2state = GPIO.input(thekeys[1])
             key3state = GPIO.input(thekeys[2])
             key4state = GPIO.input(thekeys[3])
-                
+
             if key1state == False:
                 logging.info('Cycle currencies')
                 crypto_list = currencycycle(config['ticker']['currency'])
                 config['ticker']['currency']=",".join(crypto_list)
                 lastcoinfetch=fullupdate(config, lastcoinfetch)
-                configwrite(config) 
+                configwrite(config)
             if key2state == False:
                 logging.info('Rotate - 90')
                 config['display']['orientation'] = (config['display']['orientation']+90) % 360
@@ -469,11 +472,11 @@ def main(loglevel=logging.WARNING):
                 lastcoinfetch=fullupdate(config,lastcoinfetch)
                 datapulled = True
     except IOError as e:
-        logging.info(e)
+        logging.error(e)
         image=beanaproblem(str(e)+" Line: "+str(e.__traceback__.tb_lineno))
         display_image(image)
     except Exception as e:
-        logging.info(e)
+        logging.error(e)
         image=beanaproblem(str(e)+" Line: "+str(e.__traceback__.tb_lineno))
         display_image(image)  
     except KeyboardInterrupt:    
