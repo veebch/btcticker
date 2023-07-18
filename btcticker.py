ff34f2ba3aa527197d53de184e8826c3e14d8f08
VEEB Projects <75116070+veebch@users.noreply.github.com>
2023-07-18 19:53:40 +0200

    Update btcticker.py

--- btcticker.py
+++ btcticker.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
+
+"""
+  main.py - a script for a cryptocurrency ticker.
+    
+     Copyright (C) 2023 Veeb Projects https://veeb.ch
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+"""
+
+
 from babel.numbers import decimal, format_currency, format_scientific
 from babel import Locale
 import argparse
