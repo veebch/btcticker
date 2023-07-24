90d8d924c196d135f37991f6967595624513122b
veebch <mart@veeb.ch>
2023-07-24 19:29:24 +0200

    allow multiple fiat cycle

--- btcticker.py
+++ btcticker.py
@@ -624,7 +624,7 @@ def main():
                     crypto_list = currencycycle(config['ticker']['currency'])
                     fiat_list = currencycycle(config['ticker']['fiatcurrency'])
                     config['ticker']['currency'] = ",".join(crypto_list)
-                    if config['display']['cyclefiat'] is True:
+                    if 'cyclefiat' in config['display'] and (config['display']['cyclefiat'] is True):
                         config['ticker']['fiatcurrency'] = ",".join(fiat_list)
                     # configwrite(config)
                 lastcoinfetch = fullupdate(config, lastcoinfetch)
